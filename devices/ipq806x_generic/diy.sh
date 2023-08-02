2a9c5c0d641ab97d2ad2a8c7a90e03815de8f8c4
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-08-03 04:05:04 +0800

    update

--- devices/ipq806x_generic/diy.sh
+++ devices/ipq806x_generic/diy.sh
@@ -3,3 +3,7 @@
 shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
+svn co https://github.com/coolsnowwolf/lede/trunk/package/qca package/qca
+
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-drv kmod-qca-nss-ecm-standard kmod-qca-nss-gmac kmod-qca-nss-drv-pppoe kmod-qca-mcs/' target/linux/ipq806x/Makefile
--- devices/ipq807x_generic/diy.sh
+++ devices/ipq807x_generic/diy.sh
@@ -3,9 +3,9 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-svn co https://github.com/robimarko/nss-packages/trunk/qca/qca-ssdk-shell package/network/utils/qca-ssdk-shell
+svn co https://github.com/coolsnowwolf/lede/trunk/package/qca package/qca
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += qca-ssdk-shell/' target/linux/ipq807x/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-dp kmod-qca-nss-drv-64 kmod-qca-nss-drv-pppoe-64 kmod-qca-nss-ecm-64 kmod-qca-nss-drv-bridge-mgr-64 kmod-qca-nss-drv-vlan-mgr-64 nss-firmware-ipq8074/' target/linux/ipq807x/Makefile
 
 sh -c "curl -sfL https://github.com/robimarko/openwrt/commit/23fa931934151f72c1655ffa62ff1a979575f07e.patch | patch -d './' -p1 --forward"
 
